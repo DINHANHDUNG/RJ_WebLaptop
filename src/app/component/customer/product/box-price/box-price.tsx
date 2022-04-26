dba78d4b7ef0930a99d06dc737f3f692c5622043
NAP-BN-Test <NAP-BN-Test>
2022-04-26 17:25:31 +0700

    component boxprice

---
+++ src/app/component/customer/product/box-price/box-price.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { currency } from "../../../../commom/hooks";
+
+interface propsBoxPrice {
+  value: { min_price: number; max_price: number | undefined };
+  toggle: (value: { min_price: number; max_price: number | undefined }) => void;
+}
+function BoxPrice(props: propsBoxPrice) {
+  return (
+    <div
+      className="icon-box-price"
+      style={{
+        padding: "3px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: "pointer",
+      }}
+      onClick={() => props.toggle(props.value)}
+    >
+      {!props.value.max_price ? (
+        <div
+          style={{
+            height: "66px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <span>{currency(props.value.min_price)} +</span>
+        </div>
+      ) : (
+        <>
+          <span>{currency(props.value.min_price)}</span>
+          <span>-</span>
+          <span>{currency(props.value.max_price)}</span>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default BoxPrice;
--- src/app/page/home/home.tsx
+++ src/app/page/home/home.tsx
@@ -1,8 +1,10 @@
+import { Col, Row } from "antd";
 import React, { useEffect } from "react";
 import { getProductHomePage } from "../../../features/homepage";
 import { useAppDispatch, useAppSelector } from "../../commom/hooks";
 import { productHomePageStore } from "../../commom/use-selector";
 import Banner from "../../component/customer/banner/banner";
+import BoxPrice from "../../component/customer/product/box-price/box-price";
 import SlickCarouselProduct from "../../component/slickCarousel/slickCarouselProduct";
 import SlickDetailProduct from "../../component/slickCarousel/slickDetailProduct";
 import SlickCarouselHomePage from "../../component/slickCarousel/slickProductHomepage";
@@ -14,17 +16,43 @@ function Home() {
     dispatch(getProductHomePage());
   }, []);
 
+  const arrPrice = [
+    { min_price: 2000000, max_price: 4000000 },
+    { min_price: 2000000, max_price: 4000000 },
+    { min_price: 2000000, max_price: 4000000 },
+    { min_price: 2000000, max_price: 4000000 },
+    { min_price: 2000000, max_price: 4000000 },
+    { min_price: 5000000, max_price: undefined },
+  ] as Array<{ min_price: number; max_price: number | undefined }>;
+
   console.log(products);
+
+  function toggleBoxPrice(value: {
+    min_price: number;
+    max_price: number | undefined;
+  }) {
+    console.log(value);
+  }
 
   return (
     <div className="container">
       <Banner />
       <br />
       <br />
-
+      
+      <h2 className="title title-border">LAPTOP THEO KHOẢNG GIÁ</h2>
+      <Row gutter={[16, 8]}>
+        {arrPrice.map((val) => (
+          <Col span={4}>
+            <BoxPrice value={val} toggle={(value) => toggleBoxPrice(value)} />
+          </Col>
+        ))}
+      </Row>
       {products.listproducthomepage.map((val) =>
         val.products.length > 0 ? <SlickCarouselHomePage value={val} /> : null
       )}
+
+      <br />
       {/* <SlickCarouselProduct />
 
       <SlickCarouselProduct />
