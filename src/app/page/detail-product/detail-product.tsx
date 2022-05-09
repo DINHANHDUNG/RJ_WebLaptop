8296a64f6efdd089a539570ede517041d18dd17b
NAP-BN-Test <NAP-BN-Test>
2022-05-09 15:33:49 +0700

    update banggianangcap

--- src/app/page/detail-product/detail-product.tsx
+++ src/app/page/detail-product/detail-product.tsx
@@ -179,13 +179,78 @@ function DetailProduct() {
                 aria-labelledby="product-price-link"
               >
                 <div className="product-desc-content">
-                  <h3>Bảng giá nâng cấp</h3>
                   <p>
-                    Sò Lap Xin Gửi Tới Quý Khách Hàng Bảng Giá Nâng Cấp: <br />{" "}
-                    Giá Nâng Cấp Chip: Từ i5 - 2520M lên i7 - 2620M thêm
-                    890.000đ <br /> Từ i5 - 3320M lên i7 - 3520M thêm 1.200.000đ{" "}
-                    <br /> Từ i5 - 2520M lên i7 - 2720QM thêm 1.400.000đ <br />{" "}
-                    Từ i5 - 3320M lên i7 - 3720QM thêm 1.900.000đ
+                    <h3 style={{ fontWeight: 600 }}>
+                      Sò Lap Xin Gửi Tới Quý Khách Hàng Bảng Giá Nâng Cấp:
+                    </h3>
+                    <h3 style={{ fontWeight: 600 }}>Giá Nâng Cấp Chip:</h3>
+                    Từ i5 - 2520M lên i7 - 2620M thêm 890.000đ <br /> Từ i5 -
+                    3320M lên i7 - 3520M thêm 1.200.000đ <br /> Từ i5 - 2520M
+                    lên i7 - 2720QM thêm 1.400.000đ <br /> Từ i5 - 3320M lên i7
+                    - 3720QM thêm 1.900.000đ
+                    <hr />
+                    <h3 style={{ fontWeight: 600 }}>
+                      Giá Nâng Cấp Ram (PC3-PC3L)
+                    </h3>
+                    <p>Từ 4gb lên 8gb thêm - 500.000đ</p>
+                    <p>Từ 8gb lên 16gb thêm - 850.000đ</p>
+                    <p>Từ 16gb lên 32gb thêm - 1.550.000đ</p>
+                    <hr />
+                    <h3 style={{ fontWeight: 600 }}>Ram (DDR4)</h3>
+                    <p>Từ 4gb lên 8gb thêm - 600.000đ</p>
+                    <p>Từ 8gb lên 16gb thêm - 900.000đ</p>
+                    <p>Từ 16gb lên 32gb thêm - 1.700.000đ</p>
+                    <hr />
+                    <h3 style={{ fontWeight: 600 }}>
+                      {" "}
+                      Giá Nâng Cấp Ổ Cứng (2.5 inch):
+                    </h3>
+                    <p>
+                      Từ HDD 500gb lên SSD 120gb (mới) thêm - 750.000đ nay chỉ
+                      còn 590.000đ
+                    </p>
+                    <p>
+                      Từ HDD 500gb lên SSD 240gb (mới) thêm - 1.250.000đ nay chỉ
+                      còn 850.000đ
+                    </p>
+                    <p>
+                      Từ HDD 1TB lên SSD 240gb (mới) thêm - 1.250.000đ nay chỉ
+                      còn 650.000đ
+                    </p>
+                    <p>
+                      Từ SSD 120gb (cũ) lên SSD 240gb (mới) thêm - 900.000đ nay
+                      chỉ còn 700.000đ
+                    </p>
+                    <p>
+                      Từ SSD 120gb (cũ) lên SSD 480gb (mới) thêm - 1.650.000đ
+                      nay chỉ còn 1.400.000đ
+                    </p>
+                    <p>
+                      Từ SSD 256gb (cũ) lên SSD 480gb (mới) thêm - 1.450.000đ
+                      nay chỉ còn 1.200.000đ
+                    </p>
+                    <hr />
+                    <h3 style={{ fontWeight: 600 }}>
+                      Áp dụng với ổ dạng M2 sata :
+                    </h3>
+                    <p>
+                      Từ SSD 120gb (cũ) lên SSD 240gb (mới) thêm - 1.200.000đ
+                      nay chỉ còn 800.000đ
+                    </p>
+                    <p>
+                      Từ SSD 120gb (cũ) lên SSD 480gb (mới) thêm - 1.650.000đ
+                      nay chỉ còn 1.500.000đ
+                    </p>
+                    <p>
+                      Từ SSD 256gb (cũ) lên SSD 480gb (mới) thêm - 1.450.000đ
+                      nay chỉ còn 1.300.000đ
+                    </p>
+                    <hr />
+                    <h2 style={{color: 'red'}}>
+                      {" "}
+                      Lưu ý: Giá nâng cấp áp dụng trong 15 ngày đầu kể từ khi
+                      quý khách mua máy.
+                    </h2>
                   </p>
                 </div>
               </div>
